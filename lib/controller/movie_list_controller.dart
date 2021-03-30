f0b66c37d4f2be216213b7eae832809bb68b8b59
Son <jsk9106@gmail.com>
2021-03-30 20:37:32 +0900

    version 0.2

--- lib/controller/movie_list_controller.dart
+++ lib/controller/movie_list_controller.dart
@@ -1,5 +1,50 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_movie_tmdb/models/movie.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 class MovieListController extends GetxController{
+  final ScrollController scrollController = ScrollController();
+  int page = 1;
+  RxList<dynamic> movies = [].obs;
 
+  @override
+  void onInit() {
+    super.onInit();
+    scrollController.addListener(() {
+      if(scrollController.position.pixels == scrollController.position.maxScrollExtent){
+        page++;
+        fetchData();
+      }
+    });
+  }
+
+  @override
+  void onClose(){
+    super.onClose();
+    scrollController.dispose();
+  }
+
+  Future<void> fetchData() async{
+    if(page > 50){
+      return;
+    }
+
+    String url = 'https://api.themoviedb.org/3/movie/popular?api_key=2665d2776a3a52a505d0ea4584a81f23&language=ko&page=$page';
+    try{
+      http.Response response = await http.get(url);
+
+      var body = jsonDecode(response.body);
+      List<dynamic> result = body['results'];
+      result.forEach((item) {
+        movies.add(Movie.fromJson(item));
+      });
+
+    } catch(err){
+      Get.snackbar('error', '데이터 로드 실패');
+    }
+
+  }
 }
--- lib/screens/movie_list/components/body.dart
+++ lib/screens/movie_list/components/body.dart
@@ -1,12 +1,9 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_movie_tmdb/constants.dart';
+import 'package:flutter_movie_tmdb/controller/movie_list_controller.dart';
 import 'package:flutter_movie_tmdb/models/movie.dart';
-import 'package:flutter_movie_tmdb/repository/movie_repository.dart';
 import 'package:flutter_movie_tmdb/screens/detail/detail_screen.dart';
 import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
 
 import 'movie_description.dart';
 import 'movie_poster.dart';
@@ -17,142 +14,43 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
-  final ScrollController scrollController = ScrollController();
-  List<Movie> movies = [];
-  int page = 1;
-  bool loading = false;
-
-  // Future fetchData() async {
-  //   movies = await _movieRepository.loadMovies(page);
-  //   print(movies);
-  //   return movies;
-  // }
-  //
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   scrollController.addListener(() async{
-  //     if (scrollController.position.pixels ==
-  //         scrollController.position.maxScrollExtent) {
-  //       if (page > 500) return;
-  //       page++;
-  //       nextMovie = await _movieRepository.loadMovies(page);
-  //       movies = movies + nextMovie;
-  //       print('max');
-  //     }
-  //   });
-  // }
-  //
-  // @override
-  // void dispose() {
-  //   super.dispose();
-  //   scrollController.dispose();
-  // }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return FutureBuilder(
-  //     future: fetchData(),
-  //     builder: (context, snapshot) {
-  //       if (!snapshot.hasData)
-  //         return Center(child: CircularProgressIndicator());
-  //       return Padding(
-  //         padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-  //         child: ListView.builder(
-  //           controller: scrollController,
-  //           physics: BouncingScrollPhysics(),
-  //           itemCount: movies.length,
-  //           itemBuilder: (context, index) {
-  //             Movie movie = movies[index];
-  //             return buildMovieListItem(movie);
-  //           },
-  //         ),
-  //       );
-  //     },
-  //   );
-  // }
-
-  Future<void> fetchData() async {
-    if (page > 50) {
-      return;
-    }
-
-    String url =
-        'https://api.themoviedb.org/3/movie/popular?api_key=2665d2776a3a52a505d0ea4584a81f23&language=ko&page=$page';
-    try {
-      if (page == 1) {
-        setState(() => loading = true);
-      }
-      http.Response response = await http.get(url);
-      if (page == 1) {
-        setState(() => loading = false);
-      }
-
-      final body = json.decode(response.body);
-      List<dynamic> result = body['results'];
-      result.forEach((element) {
-        movies.add(Movie.fromJson(element));
-      });
-      setState(() {});
-    } catch (err) {
-      showDialog(
-          context: context,
-          builder: (context) {
-            return AlertDialog(
-                backgroundColor: Colors.red[200],
-                title: Text('Fail'),
-                content: Text(err.toString()));
-          });
-    }
-  }
+  final MovieListController controller = Get.put(MovieListController());
 
   @override
   void initState() {
     super.initState();
-    fetchData();
-    scrollController.addListener(() {
-      if (scrollController.position.pixels ==
-          scrollController.position.maxScrollExtent) {
-        page++;
-        fetchData();
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    scrollController.dispose();
+    controller.fetchData();
   }
 
   @override
   Widget build(BuildContext context) {
-    print(movies.length);
-    return loading
-        ? Center(child: CircularProgressIndicator())
-        : Padding(
-            padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
-            child: ListView.builder(
-              controller: scrollController,
-              physics: BouncingScrollPhysics(),
-              itemCount: movies.length + 1,
-              itemBuilder: (context, index) {
-                if (index == movies.length) {
-                  if (index == 10000) {
+    return Obx(
+      () => controller.movies == null
+          ? Center(child: CircularProgressIndicator())
+          : Padding(
+              padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+              child: ListView.builder(
+                controller: controller.scrollController,
+                physics: BouncingScrollPhysics(),
+                itemCount: controller.movies.length + 1,
+                itemBuilder: (context, index) {
+                  if (index == controller.movies.length) {
+                    if (index == 10000) {
+                      return Center(
+                          child: Padding(
+                              padding: const EdgeInsets.only(top: 15),
+                              child: Text('마지막 영화입니다.')));
+                    }
                     return Center(
                         child: Padding(
                             padding: const EdgeInsets.only(top: 15),
-                            child: Text('마지막 영화입니다.')));
+                            child: CircularProgressIndicator()));
                   }
-                  return Center(
-                      child: Padding(
-                          padding: const EdgeInsets.only(top: 15),
-                          child: CircularProgressIndicator()));
-                }
-                return buildMovieListItem(movies[index]);
-              },
+                  return buildMovieListItem(controller.movies[index]);
+                },
+              ),
             ),
-          );
+    );
   }
 }
 
