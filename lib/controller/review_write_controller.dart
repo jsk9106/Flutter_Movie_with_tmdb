26fc30957e3975d0de9a511c13dd2ab8c7df9c56
Son <jsk9106@gmail.com>
2021-04-21 11:51:43 +0900

    version 0.6

--- lib/controller/review_write_controller.dart
+++ lib/controller/review_write_controller.dart
@@ -4,12 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_movie_tmdb/constants.dart';
 import 'package:get/get.dart';
 
-class ReviewWriteController extends GetxController{
+class ReviewWriteController extends GetxController {
   final TextEditingController textEditingController = TextEditingController();
   User currentUser = FirebaseAuth.instance.currentUser;
 
-  Future<void> submit({String movieTitle, double rating}) async{
-    try{
+  Future<void> submit({String movieTitle, double rating}) async {
+    try {
       await FirebaseFirestore.instance.collection('reviews').add({
         'uid': currentUser.uid,
         'content': textEditingController.text,
@@ -19,11 +19,19 @@ class ReviewWriteController extends GetxController{
         'write_time': DateTime.now(),
       });
       Get.back();
-      Get.snackbar('알림', '리뷰 작성 완료', colorText: Colors.white, backgroundColor: kButtonColor.withOpacity(0.5));
-    }
-    catch(err){
+      customSnackBar('리뷰작성 완료');
+    } catch (err) {
       print(err);
+      customSnackBar('잠시 후 다시 시도해주세요');
     }
+  }
 
+  void customSnackBar(String message) {
+    Get.snackbar(
+      '알림',
+      message,
+      colorText: Colors.white,
+      backgroundColor: kButtonColor.withOpacity(0.5),
+    );
   }
 }
